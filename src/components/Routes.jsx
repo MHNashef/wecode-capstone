79812a027fe5444704082e3238fbfe51f80a3b38
Hisham Nashef <hishamnshf@gmail.com>
2021-07-18 21:40:58 +0300

    created protected route for recipe edit

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -5,7 +5,9 @@ import RecipePage from "./RecipePage";
 import CreateRecipePage from "./CreateRecipePage";
 import Signup from "./Signup";
 import ProtectedRoute from "./ProtectedRoute";
+
 import { useAuth } from "../AuthContext";
+import { RecipeProvider } from "../RecipeContext";
 import { Switch, Route, Redirect } from "react-router-dom";
 
 export default function Routes() {
@@ -23,16 +25,43 @@ export default function Routes() {
         </Route>
         <ProtectedLogin path="/signup" auth={auth} component={Signup} />
         <ProtectedRoute path="/editUser/" auth={auth} component={Signup} />
-        <ProtectedRoute
+        <ProtectedRecipeRoute
+          path="/editRecipe/:rid"
+          auth={auth}
+          component={CreateRecipePage}
+          edit={true}
+        />
+        <ProtectedRecipeRoute
           path="/createRecipe"
           auth={auth}
           component={CreateRecipePage}
+          edit={false}
         />
       </Switch>
     </>
   );
 }
-
+const ProtectedRecipeRoute = ({
+  auth,
+  component: Component,
+  edit: Edit,
+  ...rest
+}) => {
+  return (
+    <Route
+      {...rest}
+      render={() =>
+        auth ? (
+          <RecipeProvider edit={Edit}>
+            <Component />{" "}
+          </RecipeProvider>
+        ) : (
+          <Redirect to="/" />
+        )
+      }
+    />
+  );
+};
 const ProtectedLogin = ({ auth, component: Component, ...rest }) => {
   return (
     <Route
