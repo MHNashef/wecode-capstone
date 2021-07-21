d2730cd82d93508e2da92a4f384e86bda08d446c
Hisham Nashef <hishamnshf@gmail.com>
2021-07-22 02:22:00 +0300

    added spinner to user favorites/recipes pages

--- src/components/UserFavorites.jsx
+++ src/components/UserFavorites.jsx
@@ -1,21 +1,37 @@
-import React from "react";
-import { Container } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Container, Spinner } from "react-bootstrap";
 
 export default function UserFavorites() {
+  const [spinnerOn, setSpinnerOn] = useState(true);
+  const [favoriteRecipes, setFavoriteRecipes] = useState([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setSpinnerOn(false);
+    }, 1000);
+  }, []);
+
   return (
     <>
-      <div class="jumbotron jumbotron-fluid jumbo-bg">
-        <div class="container">
-          <h1 class="display-4" style={{ fontWeight: "800" }}>
-            Your Favorites
-          </h1>
-          {/* <p class="lead">
-            This is a modified jumbotron that occupies the entire horizontal
-            space of its parent.
-          </p> */}
-        </div>
-      </div>
-      <Container></Container>
+      {spinnerOn ? (
+        <Spinner
+          className="spinner"
+          animation="border"
+          role="status"
+          variant="danger"
+        />
+      ) : (
+        <>
+          <div class="jumbotron jumbotron-fluid jumbo-bg">
+            <div class="container">
+              <h1 class="display-4" style={{ fontWeight: "800" }}>
+                Your Favorites
+              </h1>
+            </div>
+          </div>
+          <Container></Container>
+        </>
+      )}
     </>
   );
 }
